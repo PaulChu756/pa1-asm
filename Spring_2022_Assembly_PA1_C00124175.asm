cefb9cdef93a9f34c9c70fb5638a0db487cc5e14
Paul Chu <lordbabypanda@gmail.com>
2022-04-11 22:54:52 -0500

    init project

---
+++ Spring_2022_Assembly_PA1_C00124175.asm
@@ -0,0 +1,41 @@
+# Name: {Paul Chu}
+# ULID: {C00124175}
+# Course: CMPS 351-{001}
+# Assignment: Assembly PA 1 - Prime number tester
+# Certificate of Authenticity: { I certify that the code in the method function main of this project is entirely my own work.}
+# 
+#
+
+# 
+# State what registers you will use for what variables here
+# Be consistent with your statements or you will most likley get confused later on
+# 
+	.text
+	.globl main
+	
+main:
+
+# print welcome statements
+
+
+# print prompt for user input
+
+
+# get value from the user
+
+
+# use loop to determine if the numer is prime
+
+
+# print proper resulting statement 
+
+
+# print closing prompt
+
+
+# cleanly exit the program
+
+
+	.data
+welcome: 	.ascii "Welcome to the Prime Determiner!\n"
+			.asciiz "Enter some numbers and I will tell you if they are prime.\n"
